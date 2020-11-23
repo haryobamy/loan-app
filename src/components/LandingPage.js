5571383c160f64c78eafcc3bef6ac6bf35dcbfe6
haryobamy <haryobamy.badmus@gmail.com>
2020-11-23 16:01:14 +0100

    implementing the landing page for the project

---
+++ src/components/LandingPage.js
@@ -0,0 +1,283 @@
+import React from "react";
+
+// reactstrap components
+import {
+  Button,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Row,
+  Col,
+  // Carousel,
+} from "reactstrap";
+
+// core components
+import ExamplesNavbar from "./ExamplesNavbar";
+import LandingPageHeader from "./Headers/LandingPageHeader";
+import DefaultFooter from "./Footers/DarkFooter";
+import Carousel from './Carousel';
+import { orange } from "@material-ui/core/colors";
+
+function LandingPage() {
+  const [firstFocus, setFirstFocus] = React.useState(false);
+  const [lastFocus, setLastFocus] = React.useState(false);
+  React.useEffect(() => {
+    document.body.classList.add("landing-page");
+    document.body.classList.add("sidebar-collapse");
+    document.documentElement.classList.remove("nav-open");
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
+    return function cleanup() {
+      document.body.classList.remove("landing-page");
+      document.body.classList.remove("sidebar-collapse");
+    };
+  }, []);
+  return (
+    <>
+      <ExamplesNavbar />
+      <div className="wrapper">
+        <LandingPageHeader />
+        <div className="section section-about-us">
+          <Container>
+            <Row>
+            <Col md="2"></Col>
+              <Col className="ml-auto mr-auto text-center" md="8">
+                <h2 className="title">Who we are?</h2>
+                <h5 className="description">
+                <strong>Loan App</strong> is the First and Largest Entreprenuers Co-operative in the World that encourages members to consistently save. Members can enjoy being part of a community that promotes Savings and Trust. They also have access to weekly earnings, loans with no interest, collateral or paperwork, Free Business, Legal and Medical Advisory, Emergency Travel Cover and many other benefits.
+                </h5>
+              </Col>
+              <Col md="2"></Col>
+            </Row>
+            <div className="section section-how-it-works">
+            <h2 className="title">How It Works</h2>
+              <Row>
+    <Col md="4">
+    <i className="fas fa-user-edit fa-5x"></i>
+    <h3 className="title">REGISTER</h3>
+        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+        <p><a className="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+    </Col>
+        
+        <Col md="4">
+        <i className='fas fa-hand-holding-usd fa-5x'></i>
+        <h3 className='title'>CONTRIBUTE</h3>
+        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+        <p><a className="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        </Col>
+        
+        <Col md="4">
+        <i className='fa fa-money-bill-alt fa-5x'></i>
+        <h3 className='title'>CASHOUT</h3>
+        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+        <p><a className="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        </Col>
+       
+    </Row>
+    <hr />
+  </div>
+          </Container> 
+        </div>
+
+
+        <Carousel />
+
+        <div className="section section-team text-center">
+          <Container>
+            <h2 className="title">Testimonials</h2>
+            <div className="team">
+              <Row>
+                <Col md="4">
+                  <div className="team-player">
+                    <img
+                      alt="..."
+                      className="rounded-circle img-fluid img-raised"
+                      src={require("../assets/img/avatar.jpg")}
+                    ></img>
+                    <h4 className="title">Romina Hadid</h4>
+                    <p className="category text-info">Businessman</p>
+                    <p className="description">
+                      You can write here details about one of your trusted members.
+                      You can give more details about what they've achied. Feel free to
+                      add some{" "}
+                      <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                        links
+                      </a>{" "}
+                      for people to be able to follow them outside the site.
+                    </p>
+                    <Button
+                      className="btn-icon btn-round"
+                      color="info"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fab fa-twitter"></i>
+                    </Button>
+                    <Button
+                      className="btn-icon btn-round"
+                      color="info"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fab fa-instagram"></i>
+                    </Button>
+                    <Button
+                      className="btn-icon btn-round"
+                      color="info"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fab fa-facebook-square"></i>
+                    </Button>
+                  </div>
+                </Col>
+                <Col md="4">
+                  <div className="team-player">
+                    <img
+                      alt="..."
+                      className="rounded-circle img-fluid img-raised"
+                      src={require("../assets/img/ryan.jpg")}
+                    ></img>
+                    <h4 className="title">Ryan Tompson</h4>
+                    <p className="category text-info">Fashion Designer</p>
+                    <p className="description">
+                    You can write here details about one of your trusted members.
+                      You can give more details about what they've achied. Feel free to
+                      add some{" "}
+                      <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                        links
+                      </a>{" "}
+                      for people to be able to follow them outside the site.
+                    </p>
+                    <Button
+                      className="btn-icon btn-round"
+                      color="info"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fab fa-twitter"></i>
+                    </Button>
+                    <Button
+                      className="btn-icon btn-round"
+                      color="info"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fab fa-linkedin"></i>
+                    </Button>
+                  </div>
+                </Col>
+                <Col md="4">
+                  <div className="team-player">
+                    <img
+                      alt="..."
+                      className="rounded-circle img-fluid img-raised"
+                      src={require("../assets/img/eva.jpg")}
+                    ></img>
+                    <h4 className="title">Eva Jenner</h4>
+                    <p className="category text-info">software Engineer</p>
+                    <p className="description">
+                    You can write here details about one of your trusted members.
+                      You can give more details about what they've achied. Feel free to
+                      add some{" "}
+                      <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                        links
+                      </a>{" "}
+                      for people to be able to follow them outside the site.
+                    </p>
+                    <Button
+                      className="btn-icon btn-round"
+                      color="info"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fab fa-google-plus"></i>
+                    </Button>
+                    <Button
+                      className="btn-icon btn-round"
+                      color="info"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fab fa-twitter"></i>
+                    </Button>
+                  </div>
+                </Col>
+              </Row>
+            </div>
+          </Container>
+        </div>
+        <div className="section section-contact-us text-center">
+          <Container>
+            <h2 className="title">Leave A Message</h2>
+            <p className="description">Contact us now for more information, questions, etc..</p>
+            <Row>
+              <Col className="text-center ml-auto mr-auto" lg="6" md="8">
+                <InputGroup
+                  className={
+                    "input-lg" + (firstFocus ? " input-group-focus" : "")
+                  }
+                >
+                  <InputGroupAddon addonType="prepend">
+                    <InputGroupText>
+                      <i className="now-ui-icons users_circle-08"></i>
+                    </InputGroupText>
+                  </InputGroupAddon>
+                  <Input
+                    placeholder="First Name..."
+                    type="text"
+                    onFocus={() => setFirstFocus(true)}
+                    onBlur={() => setFirstFocus(false)}
+                  ></Input>
+                </InputGroup>
+                <InputGroup
+                  className={
+                    "input-lg" + (lastFocus ? " input-group-focus" : "")
+                  }
+                >
+                  <InputGroupAddon addonType="prepend">
+                    <InputGroupText>
+                      <i className="now-ui-icons ui-1_email-85"></i>
+                    </InputGroupText>
+                  </InputGroupAddon>
+                  <Input
+                    placeholder="Email..."
+                    type="text"
+                    onFocus={() => setLastFocus(true)}
+                    onBlur={() => setLastFocus(false)}
+                  ></Input>
+                </InputGroup>
+                <div className="textarea-container">
+                  <Input
+                    cols="80"
+                    name="name"
+                    placeholder="Type a message..."
+                    rows="4"
+                    type="textarea"
+                  ></Input>
+                </div>
+                <div className="send-button">
+                  <Button
+                    block
+                    className="btn-round"
+                    color="info"
+                    href="#"
+                    onClick={(e) => e.preventDefault()}
+                    size="lg"
+                  >
+                    Send Message
+                  </Button>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <DefaultFooter />
+      </div>
+    </>
+  );
+}
+
+export default LandingPage;
---
+++ src/components/SignUp.js
@@ -0,0 +1,177 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Row,
+} from "reactstrap";
+
+// core components
+import ExamplesNavbar from "./ExamplesNavbar";
+import DarkFooter from "./Footers/DarkFooter";
+
+
+
+
+
+
+function SignUp() {
+  const [firstFocus, setFirstFocus] = React.useState(false);
+  const [lastFocus, setLastFocus] = React.useState(false);
+  const [emailFocus, setEmailFocus] = React.useState(false);
+  React.useEffect(() => {
+    document.body.classList.add("Sign-page");
+    document.body.classList.add("sidebar-collapse");
+    document.documentElement.classList.remove("nav-open");
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
+    return function cleanup() {
+      document.body.classList.remove("Sign-page");
+      document.body.classList.remove("sidebar-collapse");
+    };
+  }, []);
+  return (
+    <>
+    <ExamplesNavbar />
+      <div className="section section-signup"
+        style={{
+          backgroundImage: "url(" + require("../assets/img/bg11.jpg") + ")",
+          backgroundSize: "cover",
+          backgroundPosition: "top center",
+          minHeight: "700px",
+        }}
+      >
+        <Container>
+          <Row>
+            <Card className="card-signup" data-background-color="orange">
+              <Form action="" className="form" method="">
+                <CardHeader className="text-center">
+                  <CardTitle className="title-up" tag="h3">
+                    Sign Up
+                  </CardTitle>
+                  <div className="social-line">
+                    <Button
+                      className="btn-neutral btn-icon btn-round"
+                      color="facebook"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fab fa-facebook-square"></i>
+                    </Button>
+                    <Button
+                      className="btn-neutral btn-icon btn-round"
+                      color="twitter"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                      size="lg"
+                    >
+                      <i className="fab fa-twitter"></i>
+                    </Button>
+                    <Button
+                      className="btn-neutral btn-icon btn-round"
+                      color="google"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fab fa-google-plus"></i>
+                    </Button>
+                  </div>
+                </CardHeader>
+                <CardBody>
+                  <InputGroup
+                    className={
+                      "no-border" + (firstFocus ? " input-group-focus" : "")
+                    }
+                  >
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="now-ui-icons users_circle-08"></i>
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input
+                      placeholder="First Name..."
+                      type="text"
+                      onFocus={() => setFirstFocus(true)}
+                      onBlur={() => setFirstFocus(false)}
+                    ></Input>
+                  </InputGroup>
+                  <InputGroup
+                    className={
+                      "no-border" + (lastFocus ? " input-group-focus" : "")
+                    }
+                  >
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="now-ui-icons text_caps-small"></i>
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input
+                      placeholder="Last Name..."
+                      type="text"
+                      onFocus={() => setLastFocus(true)}
+                      onBlur={() => setLastFocus(false)}
+                    ></Input>
+                  </InputGroup>
+                  <InputGroup
+                    className={
+                      "no-border" + (emailFocus ? " input-group-focus" : "")
+                    }
+                  >
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="now-ui-icons ui-1_email-85"></i>
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input
+                      placeholder="Email..."
+                      type="text"
+                      onFocus={() => setEmailFocus(true)}
+                      onBlur={() => setEmailFocus(false)}
+                    ></Input>
+                  </InputGroup>
+                </CardBody>
+                <CardFooter className="text-center">
+                  <Button
+                    className="btn-neutral btn-round"
+                    color="info"
+                    href="#pablo"
+                    onClick={(e) => e.preventDefault()}
+                    size="lg"
+                  >
+                    Register
+                  </Button>
+                </CardFooter>
+              </Form>
+            </Card>
+          </Row>
+          <div className="col text-center">
+            <Button
+              className="btn-round btn-white"
+              color="default"
+              to="/sign-in"
+              outline
+              size="lg"
+              tag={Link}
+            >
+              Login
+            </Button>
+          </div>
+        </Container>
+        <DarkFooter />
+      </div>
+    </>
+  );
+}
+
+export default SignUp;
